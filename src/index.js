ed9bdbe538f2eafec044c7c2f1ef8169a8633023
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-05-26 20:28:55 +0200

    new layout structure

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import "./style.css";
 const buttons = document.querySelectorAll("main button");
 
 buttons.forEach((button) => {
+  const star = button.parentNode.dataset.star;
   button.addEventListener("click", () => {
     const sound = button.dataset.sound;
-    handleClick(sound);
+    handleClick(star, sound);
   });
 });
 
-const handleClick = (s) => {
-  var sound = new Howl({
-    src: [`assets/${s}`],
+const handleClick = (star, sound) => {
+  const track = new Howl({
+    src: [`assets/${star}/${sound}`],
   });
 
-  sound.play();
+  track.play();
 };
